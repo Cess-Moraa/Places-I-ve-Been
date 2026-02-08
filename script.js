69a1016ad4b2f7f5fc28215f224c0d7ed39d58df
Cecilia <cessmorah42@gmail.com>
2026-02-08 19:54:03 +0300

    added titles to the constuctors and prototypes

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 //BUSINESS LOGIC
 
+// Place constructor
 function Place(location, landmarks, season, notes) {
   this.location = location;
   this.landmarks = landmarks;
   this.season = season;
   this.notes = notes;
 }
+
+//Prototype method
 Place.prototype.getLocation = function () {
   return this.location;
 };
@@ -15,20 +18,28 @@ Place.prototype.getDetails = function () {
          " | Season: " + this.season +
          " | Notes: " + this.notes;
 };
+
+// PlaceBook constructor
 function PlaceBook() {
   this.places = [];
 }
+
+// Add place
 PlaceBook.prototype.addPlace = function (place) {
   this.places.push(place);
 };
 
+// Find Place by location
 PlaceBook.prototype.findPlace = function (location) {
   return this.places.find(function (place) {
     return place.location === location;
   });
 };
+
+// Delete place by location
 PlaceBook.prototype.deletePlace = function (location) {
   this.places = this.places.filter(function (place) {
     return place.location !== location;
   });
 };
+
